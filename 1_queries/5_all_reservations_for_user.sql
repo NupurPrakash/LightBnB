09576cf6fd1daccb2f9b034f56bae01b1a9cecfe
NupurPrakash <prakashnupurprakash@gmail.com>
2020-07-22 20:12:11 +0000

    Naming convention

---
+++ 1_queries/5_all_reservations_for_user.sql
@@ -0,0 +1,10 @@
+SELECT reservations.*, properties.*, AVG(property_reviews.rating)
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON property_reviews.id = properties.id
+WHERE reservations.guest_id = 1
+AND reservations.end_date < now()::date
+GROUP BY reservations.id, properties.id
+ORDER BY reservations.start_date LIMIT 10;
+
+
